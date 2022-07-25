a41d2c1d0c850d55375fbbfcfebc7b3cfafd091e
Matteo Susanna <matteo2784@gmail.com>
2022-07-25 14:24:12 +0200

    problemi push oggetti

--- js/main.js
+++ js/main.js
@@ -2,7 +2,31 @@ var app = new Vue({
     el: '#app',
 
     data: {
-
+        input: '' ,
+        lista:  [
+                    {
+                        'text': 'fare',
+                        'done': false
+                    },
+                    {
+                        'text': 'ciao',
+                        'done': false
+                    },
+                    {
+                        'text': 'casa',
+                        'done': false
+                    },
+                    {        
+                        'text': 'casa',
+                        'done': false
+                    }
+                ]
+        
+    },
+    methods: {
+        inputText(){
+            this.lista.push(this.input);
+        }
     }
     
 })
